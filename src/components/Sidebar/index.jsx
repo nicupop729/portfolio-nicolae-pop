e808ffab6db3b689115e6bdaf833e61471cbdd5d
Nicolae Pop <nicupop729@gmail.com>
2022-04-21 19:47:33 -0400

    Build and style the sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,6 +2,12 @@ import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faLinkedin,
+  faSkype,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 import LogoN from '../../assets/images/logo-n.png';
 import LogoSubtitle from '../../assets/images/logo_sub_n.png';
 
@@ -15,13 +21,61 @@ const Sidebar = () => (
       <NavLink exact="true" activeclassname="active" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/nicolae-pop/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/nicupop729"
+        >
+          <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://twitter.com/nicupop729"
+        >
+          <FontAwesomeIcon icon={faTwitter} color="4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="skype:live:.cid.b1596767c8ac3f09"
+        >
+          <FontAwesomeIcon icon={faSkype} color="4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
