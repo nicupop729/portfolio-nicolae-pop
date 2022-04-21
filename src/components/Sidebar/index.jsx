3636c2d689514603a0b09fb42ae5de714a2fd4af
Nicolae Pop <nicupop729@gmail.com>
2022-04-21 15:51:01 -0400

    Style the sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,12 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import LogoN from '../../assets/images/logo-n.png';
+import LogoSubtitle from '../../assets/images/logo_sub_n.png';
 
 const Sidebar = () => (
-  <div className="navbar">
+  <div className="nav-bar">
     <Link className="logo" to="/">
       <img src={LogoN} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="nicolae pop" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
   </div>
 );
 
